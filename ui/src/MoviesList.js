9e141f960d2f7294f6f5de7d89aa2487a8285af6
tomek krok <tomek.krok@gmail.com>
2025-01-26 10:32:25 +0100

    avoid when title was the same so map by id - which is unique

--- ui/src/MoviesList.js
+++ ui/src/MoviesList.js
@@ -4,7 +4,8 @@ export default function MoviesList(props) {
     return <div>
         <h2>Movies</h2>
         <ul className="movies-list">
-            {props.movies.map(movie => <li key={movie.title}>
+            {/* map by id it avoid issue when title is the same */}
+            {props.movies.map(movie => <li key={movie.id}>
                 <MovieListItem movie={movie} onDelete={() => props.onDeleteMovie(movie)}/>
             </li>)}
         </ul>
