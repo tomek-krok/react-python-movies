0484269023861a3fbc24dbb4b756c48536e18fe8
Tomasz.Krok <tomek.krok@gmail.com.pl>
2025-02-17 21:36:25 +0100

    removing actor fileds

--- ui/src/MovieForm.js
+++ ui/src/MovieForm.js
@@ -16,39 +16,18 @@ export default function MovieForm(props) {
         // if (title.length < 5) {
         //     return alert('Tytuł jest za krótki');
         // }
-        // console.log('actorFields2', actorFields)
-        // actorFields.name = name;
-        // actorFields.surname = surname;
+
         props.onMovieSubmit({title, year, director, description, actors});
-        // console.log('movieIn', {title, year, director, description, actors: actorFields}); 
-        // props.onMovieSubmit({title, year, director, description});
+
         setTitle('');
         setYear('');
         setDirector('');
         setDescription('');
         setActors([]);
-        // setActorFields([]);
-        // setName('');
-        // setSurname('');
-        // console.log('movieOut', {title, year, director, description, actorFields}); 
         console.log('movieOut', {title, year, director, description});
-        //addActor(event) 
-
     }
 
-    // function addActor(event) {
-    //     event.preventDefault();
-    //     // if (name.length === 0 || surname.length === 0) {
-    //     //     return alert('Add actor name and surname, please');
-    //     // }
-    //     actorFields.name = name;
-    //     actorFields.surname = surname;
-    //     props.onAddActor(actorFields);
-    //     setName(...name, name);
-    //     setSurname(...surname, surname);
-    //     setActorFields([]);
-    //     console.log('actorOut', {actorFields});
-    // }
+    
     const handleAddActor = () => {
       const actor = { ...actorFields };
       console.log('actor', actor);
@@ -57,45 +36,50 @@ export default function MovieForm(props) {
       console.log('actorOut', actors);
     };
 
+    const handleRemoveActor = (index) => {
+      let actor = [...actors ];
+      actor.splice(index, 1);
+      setActors(actor);
+    };
+
 
 
     return <form onSubmit={addMovie}>
         <h2>Add movie</h2>
         <div>
             <label>Tytuł</label>
-            <input type="text" value={title} onChange={(event) => setTitle(event.target.value)}/>
+            <input type="text" placeholder="title more than 5 characters" value={title} onChange={(event) => setTitle(event.target.value)}/>
         </div>
         <div>
             <label>Year</label>
-            <input type="text" value={year} onChange={(event) => setYear(event.target.value)}/>
+            <input type="text" placeholder="year *" value={year} onChange={(event) => setYear(event.target.value)}/>
         </div>
         <div>
             <label>Director</label>
-            <input type="text" value={director} onChange={(event) => setDirector(event.target.value)}/>
+            <input type="text" placeholder="director *" value={director} onChange={(event) => setDirector(event.target.value)}/>
         </div>
         <div>
             <label>Description</label>
-            <textarea value={description} onChange={(event) => setDescription(event.target.value)}/>
+            <textarea value={description} placeholder="description (optional)"  onChange={(event) => setDescription(event.target.value)}/>
         </div>
-        <h3>Actors</h3>
+
+        <h3>Actors:</h3>
 
         <div >
-        <label>
-        Actors:
-        <ul>
-          {actors.map((actor, index) => (
-            <li key={index}>
-              {actor.name} {actor.surname}
-            </li>
-          ))}
-        </ul>
-      </label>
-      <label>
-        Add Actor:
-        <input type="text" value={actorFields.name} onChange={(event) => setActorFields({ ...actorFields, name: event.target.value })} />
-        <input type="text" value={actorFields.surname} onChange={(event) => setActorFields({ ...actorFields, surname: event.target.value })} />
-        <button type="button" onClick={handleAddActor}> Add </button>
-      </label>
+          <ul>
+            {actors.map((actor, index) => (
+              <li key={index}>
+                {actor.name} {actor.surname}
+                <button type="button" onClick={() => handleRemoveActor(index)}>Remove</button>
+              </li>
+           ))}
+          </ul>
+          <label>
+            Add Actor:
+            <input type="text" placeholder="name" value={actorFields.name} onChange={(event) => setActorFields({ ...actorFields, name: event.target.value })} />
+            <input type="text" placeholder="surname" value={actorFields.surname} onChange={(event) => setActorFields({ ...actorFields, surname: event.target.value })} />
+            <button type="button" onClick={handleAddActor}> Add </button>
+          </label>
         </div>
         <button>{props.buttonLabel || 'Submit'}</button>
     </form>;
